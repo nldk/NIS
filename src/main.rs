5a84d033bd5ea3b5b44292765bfafbc486af8916
Niel <nieldekeyser@gmail.com>
2025-12-10 17:27:22 +0100

    full inturpritor

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 use std::process;
 
 static stringInstructionsToU8: [&str; 27] = ["", "mov", "add", "sub", "div", "mul", "and", "or", "xor", "shr", "shl", "store", "load", "push", "pop", "jmp", "jz", "jnz", "eq", "neq", "big", "sm", "hlt", "int", "set","call","ret"];
-static stringToReg: [&str; 9] = ["r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8","sp"];
+static stringToReg: [&str; 10] = ["r0","r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8","sp"];
 #[derive(Debug)]
 struct Line {
     instruction: u8,
@@ -15,34 +15,94 @@ struct Line {
     arg2IsReg: bool,
 }
 fn getLineArgCode(arg: &str) -> (u64, bool) {
-    if arg.chars().next().unwrap() == 'r' || arg.chars().next().unwrap() == 's' {
-        return (stringToReg.iter().position(|&s| s == arg).unwrap() as u64, true);
-    } else {
-        return (arg.parse::<u64>().unwrap(), false);
-    }
-}
+    if arg.len() >0{
+        if arg.chars().next().unwrap() == 'r' || arg.chars().next().unwrap() == 's' {
+            return (stringToReg.iter().position(|&s| s == arg).unwrap() as u64, true);
+        } else {
 
+            if arg.trim().len()>1 {
+                //println!("{}", arg);
+                if arg.chars().nth(0).unwrap() == '0' || arg.chars().nth(1).unwrap() == 'x' {
+                    return match u64::from_str_radix(arg.trim_start_matches("0x"), 16) {
+                        Ok(v) => (v, false),
+                        Err(_) => {
+                            (0, false) // or return an error, or use default
+                        }
+                    };
+                }
+            }
+            return match arg.parse::<u64>() {
+                Ok(v) => (v, false),
+                Err(_) => {
+                    (0, false) // or return an error, or use default
+                }
+            };
+        }
+    }
+    return (0, false);
+
+}
+fn parse_include_file(path: &str) -> String {
+    let include_path = Path::new(path);
+    let mut include_file = File::open(include_path).unwrap();
+    let mut include_content = String::new();
+    include_file.read_to_string(&mut include_content).unwrap();
+    include_content
+}
 fn main() {
+    let mut imports:Vec<String> = vec![];
     let mut mem:Vec<u64> = vec![];
     let mut carrierbit = false;
     let mut callStack: Vec<usize> = Vec::new();
     let mut ip = 0;
-    let mut regristers: [u64; 9] = [0, 0, 0, 0, 0, 0, 0, 0,0];
+    let mut registers: [u64; 10] = [0, 0, 0, 0, 0, 0, 0, 0,0,0];
     let mut labels:HashMap<&str,u64> = HashMap::new();
     let path = Path::new("/home/niel/RustroverProjects/NISinturpriter/test.asm");
     let mut data_file = File::open(path).unwrap();
     let mut file_content = String::new();
     data_file.read_to_string(&mut file_content).unwrap();
     let split: Vec<&str> = file_content.split("\n").collect();
-    let filtered: Vec<_> = split.iter()
+    let mut filtered: Vec<&str> = split.iter()
         .filter(|line| !line.trim().is_empty())
         .cloned()
         .collect();
+    let mut filtered = filtered.iter().map(|s|s.to_string()).collect::<Vec<String>>();
     let mut instruction_index = 0;
+    for line in filtered.clone().iter() {
+        let trimmed = line.trim();
+        if trimmed.starts_with("#"){
+            let preprocessorInstruction = trimmed.split_whitespace().collect::<Vec<&str>>();
+            let instruction = preprocessorInstruction[0].trim_start_matches("#");
+            match instruction {
+                "include" =>{
+                    let path_str = preprocessorInstruction[1].to_string();
+                    let filename: String = Path::new(&path_str)
+                        .file_name()
+                        .unwrap()
+                        .to_string_lossy()
+                        .to_string();
+                    if imports.contains(&filename.clone()){
+                        continue
+                    }
+                    imports.push(filename);
+                    let include_content = parse_include_file(preprocessorInstruction[1]);
+                    let include_lines: Vec<&str> = include_content
+                        .lines()
+                        .filter(|line| !line.trim().is_empty())
+                        .collect();
+                    let include_lines: Vec<String> = include_lines.iter().map(|line|line.to_string()).collect();
+                    filtered.extend(include_lines);
+                }
+                "/"=>{continue},
+                _ => todo!(),
+            }
+        }
+    }
+    //println!("{:?}", filtered);
     for line in filtered.iter() {
         let trimmed = line.trim();
         if trimmed.starts_with("#"){
-            let instructionsplit = trimmed.split_whitespace().collect::<Vec<&str>>()[1];
+            continue;
         }
         if trimmed.ends_with(":") {
             let name = trimmed.trim_end_matches(":");
@@ -54,10 +114,15 @@ fn main() {
     }
     let mut lines: Vec<Line> = vec!();
     for (e,i) in filtered.iter().enumerate() {
+        if i.trim().starts_with(";"){
+            continue;
+        }
         if i.trim().ends_with(":") {
             continue;
         }
-
+        if i.trim().starts_with("#"){
+            continue;
+        }
         let splitLine = i.trim().split(" ").collect::<Vec<&str>>();
         if splitLine[0] == "call" || splitLine[0] == "jmp" || splitLine[0] == "jz" || splitLine[0] == "jnz" {
             if cfg!(debug_assertions) {
@@ -93,75 +158,117 @@ fn main() {
     while ip < lines.len() {
         let line = &lines[ip];
         match line.instruction {
-            1 => { regristers[line.arg1 as usize] = regristers[line.arg2 as usize] }
+            1 => { registers[line.arg1 as usize] = registers[line.arg2 as usize] }
             2 => {
                 if line.arg2IsReg {
-                    regristers[line.arg1 as usize] += regristers[line.arg2 as usize]
+                    registers[line.arg1 as usize] += registers[line.arg2 as usize]
                 } else {
-                    regristers[line.arg1 as usize] += line.arg2
+                    registers[line.arg1 as usize] += line.arg2
                 }
             }
             3 => {
                 if line.arg2IsReg {
-                    regristers[line.arg1 as usize] -= regristers[line.arg2 as usize]
+                    registers[line.arg1 as usize] -= registers[line.arg2 as usize]
                 } else {
-                    regristers[line.arg1 as usize] -= line.arg2
+                    registers[line.arg1 as usize] -= line.arg2
                 }
             }
             4 => {
                 if line.arg2IsReg {
-                    regristers[line.arg1 as usize] /= regristers[line.arg2 as usize]
+                    registers[line.arg1 as usize] /= registers[line.arg2 as usize]
                 } else {
-                    regristers[line.arg1 as usize] /= line.arg2
+                    registers[line.arg1 as usize] /= line.arg2
                 }
             }
             5 => {
                 if line.arg2IsReg {
-                    regristers[line.arg1 as usize] *= regristers[line.arg2 as usize]
+                    registers[line.arg1 as usize] *= registers[line.arg2 as usize]
                 } else {
-                    regristers[line.arg1 as usize] *= line.arg2
+                    registers[line.arg1 as usize] *= line.arg2
                 }
             }
+            6=>{
+                let dest = line.arg1;
+                let rhs = if line.arg2IsReg {
+                    registers[line.arg2 as usize]
+                } else {
+                    line.arg2
+                };
+                registers[dest as usize] &= rhs;
+            },
+            7=>{
+                let dest = line.arg1;
+                let rhs = if line.arg2IsReg {
+                    registers[line.arg2 as usize]
+                } else {
+                    line.arg2
+                };
+                registers[dest as usize] |= rhs;
+            },
+            8=>{
+                let dest = line.arg1;
+                let rhs = if line.arg2IsReg {
+                    registers[line.arg2 as usize]
+                } else {
+                    line.arg2
+                };
+                registers[dest as usize] ^= rhs;
+            },
+            9=>{
+                if line.arg2IsReg {
+                    registers[line.arg1 as usize] >>= registers[line.arg2 as usize];
+                } else {
+                    registers[line.arg1 as usize] >>= line.arg2;
+                }
+            },
+            10=>{
+                if line.arg2IsReg {
+                    registers[line.arg1 as usize] <<= registers[line.arg2 as usize];
+                } else {
+                    registers[line.arg1 as usize] <<= line.arg2;
+                }
+            },
             11 =>{
-                let addres = if line.arg1IsReg {
-                    regristers[line.arg1 as usize]
+                let address = if line.arg1IsReg {
+                    registers[line.arg1 as usize]
                 }else{
                     line.arg1
                 };
                 let value = if line.arg2IsReg {
-                    regristers[line.arg2 as usize]
+                    registers[line.arg2 as usize]
                 }else {
                     line.arg2
                 };
-                mem[addres as usize] = value;
+                mem[address as usize] = value;
             },
             12 => {
-                let addres = if line.arg2IsReg {
-                    regristers[line.arg2 as usize]
+                let address = if line.arg2IsReg {
+                    registers[line.arg2 as usize]
                 }else{
                     line.arg2
                 };
-                regristers[line.arg1 as usize] = mem[addres as usize];
+                registers[line.arg1 as usize] = mem[address as usize];
             },
             13 => {
+                //println!("rv{}", registers[9]);
                 let value = if line.arg1IsReg{
-                    regristers[line.arg1 as usize]
+                    registers[line.arg1 as usize]
                 }else {
                     line.arg1
                 } ;
-                regristers[8] += 1;
-                mem[regristers[8]as usize] = value;
+                registers[9] += 1;
+                mem[registers[9]as usize] = value;
             },
             14 => {
-                regristers[line.arg1 as usize] = mem[regristers[8] as usize];
-                regristers[8] -= 1;
+                registers[line.arg1 as usize] = mem[registers[9] as usize];
+                registers[9] -= 1;
             },
             15 =>{
                 if cfg!(debug_assertions) {
                     println!("jmping to {}", line.arg1);
                 }
                 if line.arg1IsReg {
-                    ip = regristers[line.arg1 as usize] as usize;
+                    ip = registers[line.arg1 as usize] as usize;
                 }else {
                     ip = line.arg1 as usize ;
                 }
@@ -173,7 +280,7 @@ fn main() {
                 }
                 if carrierbit {
                     if line.arg1IsReg {
-                        ip = regristers[line.arg1 as usize] as usize;
+                        ip = registers[line.arg1 as usize] as usize;
                     }else {
                         ip = line.arg1 as usize ;
                     }
@@ -182,11 +289,11 @@ fn main() {
             },
             17 => {
                 if cfg!(debug_assertions) {
-                    println!("jnz:{}",carrierbit);
+                    println!("jnz:{}", carrierbit);
                 }
                 if !carrierbit {
                     if line.arg1IsReg {
-                        ip = regristers[line.arg1 as usize] as usize;
+                        ip = registers[line.arg1 as usize] as usize;
                     }else {
                         ip = line.arg1 as usize ;
                     }
@@ -195,12 +302,12 @@ fn main() {
             },
             18 => {
                 let val1 = if line.arg1IsReg {
-                    regristers[line.arg1 as usize]
+                    registers[line.arg1 as usize]
                 }else {
                     line.arg1
                 };
                 let val2 = if line.arg2IsReg {
-                    regristers[line.arg2 as usize]
+                    registers[line.arg2 as usize]
                 }else {
                     line.arg2
                 };
@@ -208,12 +315,12 @@ fn main() {
             },
             19 => {
                 let val1 = if line.arg1IsReg {
-                    regristers[line.arg1 as usize]
+                    registers[line.arg1 as usize]
                 }else {
                     line.arg1
                 };
                 let val2 = if line.arg2IsReg {
-                    regristers[line.arg2 as usize]
+                    registers[line.arg2 as usize]
                 }else {
                     line.arg2
                 };
@@ -221,12 +328,12 @@ fn main() {
             },
             20 => {
                 let val1 = if line.arg1IsReg {
-                    regristers[line.arg1 as usize]
+                    registers[line.arg1 as usize]
                 }else {
                     line.arg1
                 };
                 let val2 = if line.arg2IsReg {
-                    regristers[line.arg2 as usize]
+                    registers[line.arg2 as usize]
                 }else {
                     line.arg2
                 };
@@ -234,32 +341,41 @@ fn main() {
             },
             21 => {
                 let val1 = if line.arg1IsReg {
-                    regristers[line.arg1 as usize]
+                    registers[line.arg1 as usize]
                 }else {
                     line.arg1
                 };
                 let val2 = if line.arg2IsReg {
-                    regristers[line.arg2 as usize]
+                    registers[line.arg2 as usize]
                 }else {
                     line.arg2
                 };
                 carrierbit = val1 < val2
             },
+            22 => {
+                let mut p:u64 = 0;
+                let mut useP:bool = false;
+                interrupt(0, 0, &mut mem, &mut p, &mut useP);
+            }
             23 => {
                 let mut p:u64 = 0;
                 let mut useP:bool = false;
                 if line.arg1IsReg {
-                    interrupt(regristers[7] as u8, regristers[line.arg1 as usize],&mut mem,&mut p,&mut useP)
+                    //println!("{}",regristers[7]);
+                    interrupt(registers[8] as u8, registers[line.arg1 as usize], &mut mem, &mut p, &mut useP)
                 } else {
-                    interrupt(regristers[7] as u8, line.arg1,&mut mem,&mut p,&mut useP)
+                    interrupt(registers[8] as u8, line.arg1, &mut mem, &mut p, &mut useP)
+                }
+                if useP{
+                    registers[7] = p
                 }
             }
-            24 => { regristers[line.arg1 as usize] = line.arg2 },
+            24 => { registers[line.arg1 as usize] = line.arg2 },
             25 => {
                 //println!("call called",);
                 callStack.push(ip+1);
                 if line.arg1IsReg {
-                    ip = regristers[line.arg1 as usize] as usize;
+                    ip = registers[line.arg1 as usize] as usize;
                 }else {
                     ip = line.arg1 as usize;
                 }
@@ -272,7 +388,7 @@ fn main() {
                     ip = return_address;
                     continue;
                 } else {
-                    panic!("RET with empty call stack");
+                    panic!("RET with empty call stack. ip: {}",ip);
                 }
                 //println!("returning to {}",ip);
                 continue;
@@ -282,7 +398,7 @@ fn main() {
         ip += 1;
     }
 }
-fn interrupt(intCode: u8, value: u64,mem: &mut Vec<u64>,p:&mut u64,useP:&mut bool) {
+fn interrupt(intCode: u8, value: u64, mem: &mut Vec<u64>, p: &mut u64, useP:&mut bool) {
     match intCode {
         0 => process::exit(value as i32),
         1 => {
@@ -293,6 +409,10 @@ fn interrupt(intCode: u8, value: u64,mem: &mut Vec<u64>,p:&mut u64,useP:&mut boo
         },
         2 => print!("{}", std::char::from_u32(value as u32).unwrap()),
         3 => print!("{}", value),
+        4 => {
+            *p = mem.len() as u64;
+            *useP = true;
+        }
         _ => panic!("invalid interrupt code {}", intCode),
     }
 }
--- test.asm
+++ test.asm
@@ -1,23 +1,29 @@
-#include test2file.asm
-eq:
-set r8 2
-set r7 74
-int r7
-jmp end
-
-neq:
-set r8 2
-set r7 78
-int r7
-jmp end
-
-end:
-set r8 0
-int r7
-
 main:
-set r1 50
-set r2 20
-neq r1 r2
-jz eq
-jnz neq
+    ; Print 'H' (ASCII 72) using int code 2
+    set r8 2
+    int 72
+
+    ; Print 'i' (ASCII 105)
+    int 105
+
+    ; Print newline (ASCII 10)
+    int 10
+
+    ; Allocate 5 slots of memory (int code 1)
+    set r8 1
+    int 5
+
+    ; Store value 123 at memory address 0
+    set r1 123
+    store 0 r1
+
+    ; Load value from address 0 back into r2
+    load r2 0
+
+    ; Print the number in r2 (int code 3)
+    set r8 3
+    int r2
+
+    ; Exit program (int code 0)
+    set r8 0
+    int 0
